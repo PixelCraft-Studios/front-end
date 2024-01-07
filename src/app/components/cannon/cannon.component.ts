a65dfd62b50affbce4e2739c3a090c31981201c1
andre <andrlzpt@protonmail.com>
2024-01-07 01:14:33 -0300

    fixed import statement

--- src/app/components/cannon/cannon.component.ts
+++ src/app/components/cannon/cannon.component.ts
@@ -1,6 +1,6 @@
 import { Component, ElementRef, inject } from '@angular/core';
 import * as THREE from 'three';
-import { Font, FontLoader } from 'three/examples/jsm/loaders/FontLoader';
+import { Font, FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
 import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
